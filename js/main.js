3e1aaeafa39d0b50b90a25af4b75d65de6e35fa1
Jasper Boyd <jasperboyd@lavabit.com>
2013-06-23 22:11:40 -0400

    Syle updates and popups in music section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 	var $artists = $('#artists'); 
 	var $artistslink = $('#artistslink'); 
+	var $white = false; 
 	
 	$artistslink.click(function(){
 		$artists.slideToggle(500); 
-		$artistslink.toggleClass('artsel');
+		if ($white) {
+			$artistslink.css('color', '#4c4681');
+			$white = false; 
+		} else {
+			$artistslink.css('color', 'white');
+			$white = true; 
+		}
 	}); 
 	
 	$artists.slideToggle(0); 
